88f4e035aad216883fadbcfe72f29e290d580d0c
mikeNegron <mikenegron14@gmail.com>
2021-11-15 12:34:03 -0400

    Linear Commit

---
+++ Linear Search.py
@@ -0,0 +1,10 @@
+example_array = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+user_input = 11
+
+for i, k in enumerate(example_array):
+    if user_input is k:
+        print(i)
+
+#i is the index
+#k is the integer value in the list
