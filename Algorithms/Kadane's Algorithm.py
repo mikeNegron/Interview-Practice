9cc68c2a9dacc7e94e5186cb96cded0b11040a0f
mikeNegron <mikenegron14@gmail.com>
2022-01-02 21:45:13 -0400

    Additional Problem

--- Algorithms/Kadane's Algorithm.py
+++ Algorithms/Kadane's Algorithm.py
@@ -1,22 +1,23 @@
 #Maximum Sum Sub-Array problem solution
 
-
 def kadane_algo(arr: list[int])-> int:
     curr_sum = arr[0]
     max_sum = curr_sum
 
-    for i in arr:
+    for i in arr[1:]:
         curr_sum = max(i + curr_sum, i)
         max_sum = max(curr_sum, max_sum)
 
     return max_sum
 
 def main():
-    test = [-2, 2, 5, -11, 6]
+    test = [5,4,-1,7,8]
 
     result = kadane_algo(test)
+    temp = bin(15)
+    print(temp[2:])
 
-    print(result)
+        
 
 if __name__ == '__main__':
     main()
---
+++ Solution Testing.py
@@ -0,0 +1,70 @@
+from functools import lru_cache
+from time import perf_counter
+
+def fib(n):
+    '''
+    Time Complexity: O(2^n)
+    Space Complexity: O(n)
+    '''
+
+    if n == 1 or n == 2: return 1
+    return fib(n - 1) + fib(n - 2)
+
+def DP_fib(n: int, mem = {}) -> int:
+    '''
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+    '''
+
+    if n == 0: return 0
+    if n in mem: return mem[n]
+        
+    else:
+        if n == 1 or n == 2: return 1
+        mem[n] = DP_fib(n - 1, mem) + DP_fib(n - 2, mem)
+            
+        return mem[n]
+
+@lru_cache
+def cache_fib(n: int) -> int:
+    '''
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+    '''
+
+    if n == 0: return 0
+    if n == 1 or n == 2: return 1
+
+    return cache_fib(n - 1) + cache_fib(n - 2)
+
+def main():
+    n = 45 #Result should be: 1,134,903,170
+
+    original_time = perf_counter()
+    original_fib = fib(n)
+    original_time_result = perf_counter() - original_time
+    
+    DP_time = perf_counter()
+    DP = DP_fib(n)
+    DP_time_result = perf_counter() - DP_time
+
+    cache_time = perf_counter()
+    cached = cache_fib(n)
+    cache_time_result = perf_counter() - cache_time
+
+    result_0 = 'True' if original_fib == 1134903170 else 'False'
+    result_1 = 'True' if DP == 1134903170 else 'False'
+    result_2 = 'True' if cached == 1134903170 else 'False'
+
+    print(f'Correct fib Result: {result_0}\nDuration: {original_time_result:.3E}s '
+          f'({original_time_result/60:.3f} mins)\n\n'
+          f'Correct DP_fib Result: {result_1}\nDuration: {DP_time_result:.3E}s\n\n'
+          f'Correct cache_fib Result: {result_2}\nDuration: {cache_time_result:.3E}s\n')
+
+    if DP_time_result < cache_time_result:
+        print(f'DP_fib was faster by: {cache_time_result - DP_time_result:.3E}s')
+    else:
+        print(f'cache_fib was faster by: {DP_time_result - cache_time_result:.3E}s')
+
+if __name__ == '__main__':
+    main()
